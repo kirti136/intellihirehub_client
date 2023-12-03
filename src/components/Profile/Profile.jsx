1e68484649f69b7a305594502301b10708331687
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-12-03 15:12:30 +0530

    updated

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,113 +1,126 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './profile.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./profile.css";
 
 const Profile = () => {
-    const [userDetails, setUserDetails] = useState(null);
-    const [editing, setEditing] = useState(false);
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-    });
-    const token = localStorage.getItem('token');
+  const [userDetails, setUserDetails] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  const token = localStorage.getItem("token");
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                if (token) {
-                    const response = await axios.get('http://localhost:5000/user-details', {
-                        headers: {
-                            Authorization: `${token}`,
-                        },
-                    });
-                    setUserDetails(response.data.user_details);
-                    setFormData({
-                        name: response.data.user_details.name,
-                        email: response.data.user_details.email,
-                    });
-                }
-            } catch (error) {
-                console.error('Error fetching user details:', error);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        if (token) {
+          const response = await axios.get(
+            "http://localhost:5000/user-details",
+            {
+              headers: {
+                Authorization: `${token}`,
+              },
             }
-        };
-
-        fetchUserDetails(); // Fetch user details only if token exists
-    }, [token]); // Dependency on token ensures this effect runs when the token changes
-
-    const handleEditOpen = () => {
-        setEditing(true);
-    };
-    const handleEditClose = () => {
-        setEditing(false);
-    };
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.patch('http://localhost:5000/user-details', formData, {
-                headers: {
-                    Authorization: `${token}`,
-                },
-            });
-            setUserDetails(response.data.user_details);
-            setEditing(false);
-        } catch (error) {
-            console.error('Error updating user details:', error);
+          );
+          setUserDetails(response.data.user_details);
+          setFormData({
+            name: response.data.user_details.name,
+            email: response.data.user_details.email,
+          });
         }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
     };
 
-    return (
-        <section className='profileSection'>
-            {token ? (
-                userDetails ? (
-                    <div className='profileDiv'>
-                        {editing ? (
-                            <form className='editForm' onSubmit={handleSubmit}>
-                                <div>
-                                    <label>Name:</label>
-                                    <input
-                                        type='text'
-                                        name='name'
-                                        value={formData.name}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div>
-                                    <label>Email:</label>
-                                    <input
-                                        type='email'
-                                        name='email'
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                                <div className='btnDiv'>
-                                    <button className='btn' type='submit'>Save</button>
-                                    <button className='btn' onClick={handleEditClose}>Close</button>
-                                </div>
-                            </form>
-                        ) : (
-                            <div className='details'>
-                                <p>ID: {userDetails.id}</p>
-                                <p>Name: {userDetails.name}</p>
-                                <p>Email: {userDetails.email}</p>
-                                <p>Role: {userDetails.role}</p>
-                                <button className='btn' onClick={handleEditOpen}>Edit</button>
-                            </div>
-                        )}
-                    </div>
-                ) : (
-                    <p>Loading user details...</p>
-                )
+    fetchUserDetails(); // Fetch user details only if token exists
+  }, [token]); // Dependency on token ensures this effect runs when the token changes
+
+  const handleEditOpen = () => {
+    setEditing(true);
+  };
+  const handleEditClose = () => {
+    setEditing(false);
+  };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.patch(
+        "http://localhost:5000/user-details",
+        formData,
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      setUserDetails(response.data.user_details);
+      setEditing(false);
+    } catch (error) {
+      console.error("Error updating user details:", error);
+    }
+  };
+
+  return (
+    <section className="profileSection">
+      {token ? (
+        userDetails ? (
+          <div className="profileDiv">
+            {editing ? (
+              <form className="editForm" onSubmit={handleSubmit}>
+                <div>
+                  <label>Name:</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="btnDiv">
+                  <button className="btn" type="submit">
+                    Save
+                  </button>
+                  <button className="btn" onClick={handleEditClose}>
+                    Close
+                  </button>
+                </div>
+              </form>
             ) : (
-                <p>Please login to view your profile.</p>
+              <div className="details">
+                <p>ID: {userDetails.id}</p>
+                <p>Name: {userDetails.name}</p>
+                <p>Email: {userDetails.email}</p>
+                <p>Role: {userDetails.role}</p>
+                <button className="btn" onClick={handleEditOpen}>
+                  Edit
+                </button>
+              </div>
             )}
-            <hr />
-        </section>
-    );
+          </div>
+        ) : (
+          <p>Loading user details...</p>
+        )
+      ) : (
+        <p>Please login to view your profile.</p>
+      )}
+      <hr />
+    </section>
+  );
 };
 
 export default Profile;
